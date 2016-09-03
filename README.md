845ee9d2d6c88f0a0a1a2ae80911789a18745aac
Terry <gidcai@gmail.com>
2016-09-03 20:40:20 +0800

    Site updated at 2016-09-03 12:40:20 UTC

--- README.md
+++
@@ -1,4 +0,0 @@
-my blog
-
-
-http://blog.w3cub.com
--- assets/javascripts/tools/html_js.js
+++
@@ -1,6 +0,0 @@
-﻿ function rechange(){
- document.getElementById('re').value=document.getElementById('oresult').value.replace(/document.writeln\("/g,"").replace(/"\);/g,"").replace(/\\\"/g,"\"").replace(/\\\'/g,"\'").replace(/\\\//g,"\/").replace(/\\\\/g,"\\")
- }
- function change(){
- document.getElementById('re').value="document.writeln(\""+document.getElementById('oresult').value.replace(/\\/g,"\\\\").replace(/\\/g,"\\/").replace(/\'/g,"\\\'").replace(/\"/g,"\\\"").split('\n').join("\");\ndocument.writeln(\"")+"\");"
- }
